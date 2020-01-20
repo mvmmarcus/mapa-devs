f5e9fd695bfebcdb7fcfbad973f2b9ed629edba1
MARCUS VINÍCIUS MARQUES <marcus.vinicius.marques@hotmail.com>
2020-01-20 19:40:27 -0300

    Adicionando a aplicação

---
+++ web/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './global.css';
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <asside>
+        <strong>Cadastrar</strong>
+        <form>
+          <label>Usuário do Github</label>
+          <input></input>
+        </form>
+      </asside>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
