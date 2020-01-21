f2983acae44189a186eeae855d98e2d9f412fec2
MARCUS VINÍCIUS MARQUES <marcus.vinicius.marques@hotmail.com>
2020-01-20 23:00:47 -0300

    Adicionando estilização CSS

--- web/src/App.js
+++ web/src/App.js
@@ -2,19 +2,84 @@ import React from 'react';
 
 import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
     <div id="app">
-      <asside>
+      <aside>
         <strong>Cadastrar</strong>
         <form>
-          <label>Usuário do Github</label>
-          <input></input>
+          <div className="input-block">
+            <label htmlFor="github_username" >Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs" >Tecnologias</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude" >Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude" >Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+          
+          <button type="submit">Salvar</button>
         </form>
-      </asside>
+      </aside>
       <main>
-
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/51442820?s=460&v=4" alt="Marcus"/>
+              <div className="user-info">
+                <strong>Marcus vinicius</strong>
+                <span>Javascript, NodeJs</span>
+              </div>
+            </header>
+            <p>Estudante de engenharia de computação, empolgado com desenvolvimento de aplicações</p>
+            <a href="https://github.com/mvmmarcus/">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/51442820?s=460&v=4" alt="Marcus"/>
+              <div className="user-info">
+                <strong>Marcus vinicius</strong>
+                <span>Javascript, NodeJs</span>
+              </div>
+            </header>
+            <p>Estudante de engenharia de computação, empolgado com desenvolvimento de aplicações</p>
+            <a href="https://github.com/mvmmarcus/">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/51442820?s=460&v=4" alt="Marcus"/>
+              <div className="user-info">
+                <strong>Marcus vinicius</strong>
+                <span>Javascript, NodeJs</span>
+              </div>
+            </header>
+            <p>Estudante de engenharia de computação, empolgado com desenvolvimento de aplicações</p>
+            <a href="https://github.com/mvmmarcus/">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/51442820?s=460&v=4" alt="Marcus"/>
+              <div className="user-info">
+                <strong>Marcus vinicius</strong>
+                <span>Javascript, NodeJs</span>
+              </div>
+            </header>
+            <p>Estudante de engenharia de computação, empolgado com desenvolvimento de aplicações</p>
+            <a href="https://github.com/mvmmarcus/">Acessar perfil do Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
