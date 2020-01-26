5be589c827b64bd49b931a75021431d76f560844
MARCUS VINÍCIUS MARQUES <marcus.vinicius.marques@hotmail.com>
2020-01-25 22:07:19 -0300

    Adicionando Geolocation

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import Routes from './src/Routes';
+import { StatusBar } from 'react-native';
 
 function App() {
   return (
+    <>
+    <StatusBar barStyle="light-content" backgroundColor="#7D40E7"></StatusBar>
     <Routes></Routes>
+    </>
   );
 }
 
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { View } from 'react-native';
-import Routes from '../Routes';
+import MapView from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, PermissionsAndroid } from 'react-native';
 
-function Main() {
-    return (
-        <View>
-            
-        </View>
-    )
+import Geolocation from 'react-native-geolocation-service';
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+});
+
+function Main() {  
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+        try {
+            const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                Geolocation.getCurrentPosition(
+                    position => {     
+                      setCurrentRegion({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        latitudeDelta: 0.004,
+                        longitudeDelta: 0.004,
+                      });
+                    },
+                    error => {
+                      console.log(error.code, error.message);
+                    },
+                    {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
+                  );
+            }
+          } catch (err) {
+            console.warn(err);
+          }
+        }
+    console.log(currentRegion);
+    loadInitialPosition();
+  }, []);
+  
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+
+    </MapView>
+  );
 }
 
 export default Main;
