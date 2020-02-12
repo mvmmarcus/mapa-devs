e195130a6268f6c0d0d4cb8cfac1fce37645956c
Marcus Vinicius Marques <51442820+mvmmarcus@users.noreply.github.com>
2020-02-12 07:27:03 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb://mvmmarcus:Mvm131296@cluster0-shard-00-00-jcjdh.mongodb.net:27017,cluster0-shard-00-01-jcjdh.mongodb.net:27017,cluster0-shard-00-02-jcjdh.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('mongodb://mvmmarcus:<password>@cluster0-shard-00-00-jcjdh.mongodb.net:27017,cluster0-shard-00-01-jcjdh.mongodb.net:27017,cluster0-shard-00-02-jcjdh.mongodb.net:27017/<schema>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
